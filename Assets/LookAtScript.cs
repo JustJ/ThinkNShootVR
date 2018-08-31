af23a59d22af83d74ce6d802d797ea231a69d6cc
JustJ <Ameglin.j@gmail.com>
2018-09-01 00:09:59 +0300

    Добавлена фича: "Текст направлен всегда на игрока"

---
+++ Assets/LookAtScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtScript : MonoBehaviour {
+
+    public Transform lookTarget;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(lookTarget != null)
+        {
+            var offset = 2 * transform.position - lookTarget.position;
+
+            transform.LookAt(offset);
+        }
+	}
+}
